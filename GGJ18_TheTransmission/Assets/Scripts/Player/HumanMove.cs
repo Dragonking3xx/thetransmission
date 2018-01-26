f1fea41c0537fbadacca863f9e0d354c7184eee3
Christian Afonso <chr.afonso@gmail.com>
2018-01-26 23:55:03 +0100

    HumanMove: player can move left and right

--- GGJ18_TheTransmission/Assets/Scripts/Player/HumanMove.cs
+++ GGJ18_TheTransmission/Assets/Scripts/Player/HumanMove.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ *	Basic moves, just walk
+ */
 public class HumanMove : MonoBehaviour {
+
+	public float Speed = 5;
+
+	private Vector2 inputVector;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +18,34 @@ public class HumanMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// TODO handle indirectly via InputController
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			inputVector.x = -1;
+		}
+		else if(Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			if (!Input.GetKey(KeyCode.RightArrow))
+			{
+				inputVector.x = 0;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			inputVector.x = 1;
+		}
+		else if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			if (!Input.GetKey(KeyCode.LeftArrow))
+			{
+				inputVector.x = 0;
+			}
+		}
+
+		Vector2 pos = transform.position;
+
+		pos += (inputVector * Speed * Time.deltaTime);
+
+		transform.position = pos;
 	}
 }
