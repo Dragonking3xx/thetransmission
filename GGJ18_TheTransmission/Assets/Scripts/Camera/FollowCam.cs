377be1bc5f596f20234299ebe933ae8d3134295d
Christian Afonso <chr.afonso@gmail.com>
2018-01-26 23:21:50 +0100

    Added dummy FloorMaterial, simple FollowCam script

---
+++ GGJ18_TheTransmission/Assets/Scripts/Camera/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+	public GameObject FollowTarget;
+
+	// TODO smoothing speed etc.
+	// TODO contraints?
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FollowTarget != null)
+		{
+			Vector3 pos = transform.position;
+
+			pos.x = FollowTarget.transform.position.x;
+			pos.y = FollowTarget.transform.position.y;
+			// TODO smooth
+
+			transform.position = pos;
+		}
+	}
+}
