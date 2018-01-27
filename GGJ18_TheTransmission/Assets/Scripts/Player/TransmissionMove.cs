5b19feb6dce6339e6d7c4b8cbb8310a0f02116d4
Dragonking3xx <32482624+Dragonking3xx@users.noreply.github.com>
2018-01-27 11:16:42 +0100

    transmission move
    added transmission move

--- GGJ18_TheTransmission/Assets/Scripts/Player/TransmissionMove.cs
+++ GGJ18_TheTransmission/Assets/Scripts/Player/TransmissionMove.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+    private Rigidbody rb;
+    public float speed = 10;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+
+
 }
