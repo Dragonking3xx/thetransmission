e975ab93f765b1576691eb125fbc3106b4fa445c
Christian Afonso <chr.afonso@gmail.com>
2018-01-27 13:32:59 +0100

    Transmission/HumanMove inherit from Move, Transmit/receive works, created Level/Item/Action scripts, still empty

---
+++ GGJ18_TheTransmission/Assets/Scripts/Player/Move.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Abstract Base class for different moves
+public abstract class Move : MonoBehaviour {
+	// derive and fill
+
+	public abstract void Activate();
+	public abstract void Deactivate();
+
+}
--- GGJ18_TheTransmission/Assets/Scripts/Player/TransmissionMove.cs
+++ GGJ18_TheTransmission/Assets/Scripts/Player/TransmissionMove.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class TransmissionMove : Move {
 
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     public float speed = 10;
+
+	private GameObject humanInRange = null;
 
 	// Use this for initialization
 	void Start ()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 
@@ -25,6 +28,72 @@ public class PlayerMove : MonoBehaviour {
 
     }
 
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "human")
+		{
+			Debug.Log("human in range: " + other.name);
+			humanInRange = other.gameObject;
+		}
+	}
 
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "human" && humanInRange == other.gameObject)
+		{
+			Debug.Log("human out of range: " + other.name);
+			humanInRange = null;
+		}
+	}
 
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.T) || Input.GetButton("Jump"))
+		{
+			TryReceive();
+		}
+	}
+
+	void TryReceive()
+	{
+		if(humanInRange)
+		{
+			humanInRange.SendMessage("Receive", gameObject);
+		}
+	}
+
+	public override void Activate()
+	{
+		this.enabled = true;
+
+		GameObject charactersNode = GameObject.Find("Characters");
+		if (charactersNode != null)
+		{
+			transform.parent = charactersNode.transform;
+		}
+		else
+		{
+			Debug.Log("Could not find the level's Characters Node!");
+			transform.parent = null; // global root?
+		}
+
+		// TODO activation animation
+		GetComponent<Renderer>().enabled = true;
+	}
+
+	public override void Deactivate()
+	{
+		this.enabled = false;
+
+		if (humanInRange != null) {
+			transform.parent = humanInRange.transform;
+		}
+		else
+		{
+			Debug.Log("Nu humanInRange found, can't set parent!");
+		}
+
+		// TODO deactivating animation
+		GetComponent<Renderer>().enabled = false;
+	}
 }
