b51fa7c070aab2d6c516b0e208d9f94b8e911ace
Christian Afonso <chr.afonso@gmail.com>
2018-01-27 21:39:48 +0100

    Added GameController prefab, added states (not doing much yet)

--- GGJ18_TheTransmission/Assets/Scripts/Game/GameController.cs
+++ GGJ18_TheTransmission/Assets/Scripts/Game/GameController.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+
+	private enum State { TITLE, INGAME, DIALOGUE, DEATH };
+	private State currentState = State.INGAME;
 
 	private static GameController instance;
 	public static GameController Instance
@@ -42,23 +46,64 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		DontDestroyOnLoad(gameObject); // stay persistent -- needed? On Die/Respawn everything should be reset
 
-        Player = GameObject.FindGameObjectWithTag("transmission");
-    }
+		ResetCurrentLevel();
+	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void ResetCurrentLevel()
+	{
+		Player = GameObject.FindGameObjectWithTag("transmission");
+		FollowCam followCam = Camera.main.gameObject.AddComponent<FollowCam>();
+		followCam.FollowTarget = Player;
 	}
 
-    public void DialogEnd()
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void DialogEnd()
     {
 
     }
+
     public void Death(string DeathText)
     {
-
+		SetState(State.DEATH);
 
         NormalTextBox.Instance.Death(DeathText);
     }
+
+	private void SetState(State newState)
+	{
+		switch(newState)
+		{
+			case State.TITLE:
+				// TODO
+				break;
+			case State.INGAME:
+				SetPlayerInteractionEnabled(true);
+				break;
+			case State.DIALOGUE:
+				SetPlayerInteractionEnabled(false);
+				break;
+			case State.DEATH:
+				SetPlayerInteractionEnabled(false);
+				break;
+		}
+	}
+
+	private void SetPlayerInteractionEnabled(bool enabled)
+	{
+		if(Player != null)
+		{
+			Player.GetComponent<Move>().enabled = enabled;
+			foreach(Action action in Player.GetComponents<Action>())
+			{
+				action.enabled = enabled;
+			}
+		}
+	}
 }
